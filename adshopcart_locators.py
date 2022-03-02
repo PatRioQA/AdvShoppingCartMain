7d68a8995c87a6f30f5087b3d3f52dd71db76feb
PatRioQA <99362974+PatRioQA@users.noreply.github.com>
2022-03-01 18:47:52 -0800

    Update adshopcart_locators.py

--- adshopcart_locators.py
+++ adshopcart_locators.py
@@ -1,4 +1,10 @@
+from faker import Faker
 
-
-
+fake = Faker(locale='en_CA')
 adshopcart_url = 'https://advantageonlineshopping.com/#/'
+new_username = fake.user_name()
+new_password = fake.password()
+first_name = fake.first_name()
+last_name = fake.last_name()
+email = fake.email()
+phone = fake.phone_number()
