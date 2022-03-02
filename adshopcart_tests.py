2c6aa7549b6431214edadef5be32500971c522ad
PatRioQA <99362974+PatRioQA@users.noreply.github.com>
2022-03-01 18:39:01 -0800

    Update adshopcart_tests.py

--- adshopcart_tests.py
+++ adshopcart_tests.py
@@ -1,3 +1,14 @@
-find_title()
-setUp()
-tearDown()
+import adshopcart_methods as methods
+import adshopcart_locators as locators
+import unittest
+
+
+class AdshopcartPositiveTestCases(unittest.TestCase):
+
+    def test_adshopcart(self):
+        setUp()
+        create_new_user()
+        log_out()
+        log_in()
+        delete_account()
+        tearDown()
